d2351766e0f3bf6839223e71ed3f6c4b54303b7e
Anthony de Cuyper <decuyperanthony@gmail.com>
2020-11-20 12:59:35 +0100

    map on soldiers in cards component :+1:

--- client/src/App/HomePage/Cards.js
+++ client/src/App/HomePage/Cards.js
@@ -1,7 +1,8 @@
 import React from 'react';
-
+import { useSelector, useDispatch } from 'react-redux';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card';
 
 const useStyles = makeStyles((theme) => ({
 //   card: {
@@ -16,33 +17,41 @@ const useStyles = makeStyles((theme) => ({
     borderRadius: '4px',
     textAlign: 'left',
     padding: '0.5em',
+    fontWeight: 'bold',
+  },
+
+  cardContainer: {
+    padding: '0.5em',
   },
 
 }));
 
 const Cards = () => {
   const classes = useStyles();
+  const { soldiers } = useSelector((state) => state);
+  //   console.log('soldiers', soldiers);
+  let soldiersJSX;
+  if (soldiers.results) {
+    soldiersJSX = soldiers.results.map((s) => {
+      console.log('s', s);
+      return (
+        <Grid
+
+          key={s.name}
+          item
+          xs={12}
+          sm={6}
+          md={4}
+        >
+          <Card className={classes.cardContainer}>{s.name}</Card>
+        </Grid>
+      );
+    });
+  }
   return (
     <div className={classes.cardsContainer}>
       <Grid container spacing={2}>
-        <Grid item xs={12} sm={6} md={4}>
-          hello
-        </Grid>
-        <Grid item xs={12} sm={6} md={4}>
-          hello
-        </Grid>
-        <Grid item xs={12} sm={6} md={4}>
-          hello
-        </Grid>
-        <Grid item xs={12} sm={6} md={4}>
-          hello
-        </Grid>
-        <Grid item xs={12} sm={6} md={4}>
-          hello
-        </Grid>
-        <Grid item xs={12} sm={6} md={4}>
-          hello
-        </Grid>
+        {soldiersJSX}
       </Grid>
     </div>
   );
