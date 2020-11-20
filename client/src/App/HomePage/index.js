bf3e00645ab23e5b13de72d6029abaccf2fa0173
Anthony de Cuyper <decuyperanthony@gmail.com>
2020-11-20 11:34:54 +0100

    Update index.js
    update style for homepage index

--- client/src/App/HomePage/index.js
+++ client/src/App/HomePage/index.js
@@ -2,20 +2,32 @@ import React from 'react';
 
 import { makeStyles } from '@material-ui/core/styles';
 
+// === component
+import SearchBar from './SearchBar';
+
 const useStyles = makeStyles((theme) => ({
-  card: {
-    display: 'flex',
-    [theme.breakpoints.down('xs')]: {
-      width: '250px',
-    },
+  blocHomePage: {
+    maxWidth: '800px',
+    margin: 'auto',
+    backgroundColor: '#ECF0F1',
+    padding: '1em auto',
   },
+  //   card: {
+  //     display: 'flex',
+  //     [theme.breakpoints.down('xs')]: {
+  //       width: '250px',
+  //     },
+  //   },
 
 }));
 
 const HomePage = () => {
   const classes = useStyles();
   return (
-    <div>HomePage</div>
+    <div className={classes.blocHomePage}>
+      <h1 style={{ margin: 0 }}>Star wars</h1>
+      <SearchBar />
+    </div>
   );
 };
 
