edd2ea0e920925c315db9fce70dfd3ff2d5274aa
Anthony de Cuyper <decuyperanthony@gmail.com>
2020-11-20 12:31:58 +0100

    method to get all soldiers ok

--- server/app/controllers/homeController.js
+++ server/app/controllers/homeController.js
@@ -1,9 +1,26 @@
 const API_URL = process.env.SWAPI_API;
+const axios = require('axios');
 
 const homeController = {
     homeApi: async (req, res) => {
         try {
             res.send('Hello, welcome on Star Wars API');
+        } catch (error) {
+            console.trace(error);
+            res.status(500).send(error);
+        }
+    },
+
+    getAllSoldiers: async (req, res) => {
+        try {
+            let soldiers;
+            await axios.get(`${API_URL}/people`)
+                 .then((res) => {
+                     soldiers = res.data;
+                 })
+                 .catch(err => console.trace(err));
+                 console.log('soldiers', soldiers)
+                res.send(soldiers);
         } catch (error) {
             console.trace(error);
             res.status(500).send(error);
