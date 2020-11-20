727600469b400dadbaabcff729acd31b56f0003e
Anthony de Cuyper <decuyperanthony@gmail.com>
2020-11-20 10:32:56 +0100

    react router with all route done

--- client/src/App/App.js
+++ client/src/App/App.js
@@ -1,12 +1,42 @@
 import React from 'react';
+
+import {
+  Route,
+  Switch,
+  Redirect,
+  useRouteMatch,
+} from 'react-router-dom';
+
 import './App.css';
+
+//* == component
 import Login from './Login';
+import HomePage from './HomePage';
 
 const App = () => (
   <div className="App">
-    APP
-    <p>blabla</p>
-    <Login />
+    <Switch>
+      <Route exact path="/login">
+        <Login />
+      </Route>
+
+      {/* <Route
+          exact
+          path={`${path}/profile`}
+          render={(props) => {
+            if (!userToken) {
+              return <Redirect to="/" />;
+            }
+            // eslint-disable-next-line react/jsx-props-no-spreading
+            return <Profile data={user} />;
+          }}
+        /> */}
+
+      <Route path="/">
+        <HomePage />
+      </Route>
+    </Switch>
+
   </div>
 );
 
